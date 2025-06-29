1c3e190f33422b1ddae399e3165f8a0120c3b8e0
Saksham Ojha <sakshamojha2006@outlook.com>
2025-06-29 18:36:14 +0530

    Add project files.

---
+++ TDD.Domain.Tests/CalculatorTests.cs
@@ -0,0 +1,18 @@
+﻿namespace TDD.Domain.Tests
+{
+    public class CalculatorTests
+    {
+        [Fact]
+        public void Add_ReturnsSumOfTwoNumbers()
+        {
+            //Arrange
+            var calculator = new Calculator();
+
+            //Act
+            var result = calculator.Add(5, 10);
+
+            //Assert
+            Assert.Equal(15, result);
+        }
+    }
+}
---
+++ TDD.Domain/Calculator.cs
@@ -0,0 +1,10 @@
+﻿namespace TDD.Domain
+{
+    public class Calculator
+    {
+        public int Add(int a, int b)
+        {
+            return a + b + 1;
+        }
+    }
+}
